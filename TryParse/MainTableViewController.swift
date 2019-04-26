c99a0f8c13a731bad128ecb5cb8d14349112dba7
RafaCosin <RafaCosin@users.noreply.github.com>
2019-04-26 12:17:45 +0200

    infor_tableV_con_descripcion_foto_dos_funcione_escape

--- TryParse/MainTableViewController.swift
+++ TryParse/MainTableViewController.swift
@@ -11,61 +11,66 @@ import UIKit
 class MainTableViewController: UITableViewController {
     
      var movie = [Movie]()
-    
-    
-        override func viewDidLoad() {
+     var imgPaso = UIImage()
+    var arrayImage = [UIImage]()
+     override func viewDidLoad() {
             super.viewDidLoad()
             getJSON(movieCompletionHandler: {
                  (movie, error) in
                 if let movie = movie {
                     self.movie = movie
-                    
+                     self.tableView.reloadData()
                 }
-            })
-            
+             })
         }
-//        override func viewWillAppear(_ animated: Bool) {
-//             tableView.reloadData()
-//        }
-        
+
     func getJSON(movieCompletionHandler:  @escaping ([Movie]?,Error?) -> Void) {
         
             let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=7a312711d0d45c9da658b9206f3851dd&language=en-US&page=1")
             
             let task = URLSession.shared.dataTask(with: url!,completionHandler: { (data, response, error) in
+                
                 guard let data = data,
                     error == nil else {
                         print(error?.localizedDescription ?? "Response Error")
                         return }
                 do {
-                    
                     let decoder = JSONDecoder()
-                    let user = try! decoder.decode(MovieResults.self, from: data)
-                    
-                    //print(user.results[0].title)
-
+                    let user = try decoder.decode(MovieResults.self, from: data)
                     DispatchQueue.main.async {
-                        
                         movieCompletionHandler(user.results, nil)
-                        self.tableView.reloadData()
+                       
                     }
-                    
                 } catch  let parseErr {
                     print("Error" )
                     DispatchQueue.main.async {
-                        
-                   
-                    movieCompletionHandler(nil, parseErr)
-                         }
+                        movieCompletionHandler(nil, parseErr)
+                    }
                 }
-               
             })
-            
             task.resume()
+        }
+    }
+ func imageFromServerURL(url: URL, imgcompletionHandler: @escaping (UIImage?, Error?) -> Void) {
+    //print("en funcion : \(url)")
+    let task = URLSession.shared.dataTask(with: url,completionHandler: { (data, response, error) in
+        
+        if error != nil {
+            DispatchQueue.main.async {
+                imgcompletionHandler(nil,error)
+            }
+        }
+        guard let datos = data else {return}
+        DispatchQueue.main.async {
+            print("data : dispatch)")
+            let image = UIImage(data: datos)
+            
+            imgcompletionHandler(image,nil)
             
         }
-    
-    }
+    }).resume()
+}
+
 
 extension MainTableViewController {
  
@@ -78,15 +83,30 @@ extension MainTableViewController {
 
 
  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
- let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TryTableViewCell
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TryTableViewCell
     let task = movie[indexPath.row]
-    cell.overviewLabel.text = task.title
+    
+    
+    cell.overviewLabel.text = task.overview
     cell.titleLabel.text = task.title
- 
 
- 
- return cell
+    let imgFixed = "https://image.tmdb.org/t/p/w154"
+    let imageURL = task.posterPath
+    let completa = URL(string: imgFixed + imageURL)!
+  
+    //"w92", "w154", "w185", "w342", "w500", "w780", or "original"
+    imageFromServerURL(url: completa, imgcompletionHandler: {(image,error) in
+        if let imagen = image {
+              let pepe = imagen
+            DispatchQueue.main.async {
+                print("async en cell: dispatch)")
+                cell.img.image = pepe
+            }
+        }
+    })
+    return cell
  }
-
+ 
 }
 
